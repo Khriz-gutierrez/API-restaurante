c448fd90367ad8092a057369b5d0ef8a51bff644
Khriz Gutierrez <est.kjgutierrezo0201@uml.edu.ni>
2025-05-14 22:06:17 -0600

    Corregir la ruta de importación para historialPedidosController

--- src/routes/historialPedidosRouter.js
+++ src/routes/historialPedidosRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { listarHistorialPedido } from '../../controllers/historialPedidosController.js';  // Corregir la ruta
+import { listarHistorialPedido } from '../../controllers/historialPedidosController.js';
 
 const router = Router();
 
